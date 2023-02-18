82f272aca6d523f6964500d5524fa2e9f89cb869
kristinayax <kristinayax@icloud.com>
2023-02-18 19:31:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 5000; // стоимость авиабилета
+        int bonus = 20; // количество рублей для 1 бонусной мили
+
+        System.out.println("Количество бонусных миль к начислению:");
+        System.out.println(amount / bonus);
+
+
+    }
+}
